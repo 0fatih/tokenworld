9ccb9eace80661184841e01cd9e9ef20e3035fce
Fatih <fatihfurkn@gmail.com>
2023-07-21 10:54:30 +0300

    feat: :fire: Add `burn` function

--- x/tokenworld/keeper/msg_server_burn.go
+++ x/tokenworld/keeper/msg_server_burn.go
@@ -5,13 +5,23 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"tokenworld/x/tokenworld/types"
+
+  "cosmossdk.io/errors"
 )
 
 func (k msgServer) Burn(goCtx context.Context, msg *types.MsgBurn) (*types.MsgBurnResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	sender, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return nil, errors.Wrapf(types.ErrBurn, "invalid receiver address %v", msg.Creator)
+	}
+
+	// Take coins from the recipient
+	err = k.bk.SendCoinsFromAccountToModule(ctx, sender, types.ModuleName, msg.Amount)
+	if err != nil {
+    return nil, errors.Wrapf(types.ErrReceiveCoins, "taking %v tokens from %s failed", msg.Amount, sender)
+	}
 
 	return &types.MsgBurnResponse{}, nil
 }
--- x/tokenworld/types/errors.go
+++ x/tokenworld/types/errors.go
@@ -8,6 +8,8 @@ import (
 
 // x/tokenworld module sentinel errors
 var (
-	ErrMint     = errors.Register(ModuleName, 1100, "mint coins failed")
-	ErrSendMint = errors.Register(ModuleName, 1101, "send minted coins failed")
+	ErrMint         = errors.Register(ModuleName, 1100, "mint coins failed")
+	ErrSendMint     = errors.Register(ModuleName, 1101, "send minted coins failed")
+	ErrBurn         = errors.Register(ModuleName, 1102, "burn coins failed")
+	ErrReceiveCoins = errors.Register(ModuleName, 1103, "receive coins failed")
 )
