1dc2db9c035bb19526efa583925f86ba0a518be2
Fatih <fatihfurkn@gmail.com>
2023-07-21 10:03:48 +0300

    feat: :fire: Mint function added

--- x/tokenworld/types/errors.go
+++ x/tokenworld/types/errors.go
@@ -3,10 +3,11 @@ package types
 // DONTCOVER
 
 import (
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	errors "cosmossdk.io/errors"
 )
 
 // x/tokenworld module sentinel errors
 var (
-	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrMint = errors.Register(ModuleName, 1100, "mint coins failed")
+	ErrSendMint = errors.Register(ModuleName, 1101, "send minted coins failed")
 )
